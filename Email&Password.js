ffb127ec12c56ec9244fb817590bd3b6cc257eae
RaphaelB <raphael.bonacina@hotmail.fr>
2024-07-28 14:21:03 +0200

    css html + email password post

--- Email&Password.js
+++ Email&Password.js
@@ -1,9 +1,29 @@
 export function Emailbutton (){
-    //fetch('http://localhost:5678/api/users/login', {
-     //   method:"POST",
-       // body:"{email}",
-        //headers: { "Content-Type": "application/json" }
-    //}
+    const formulaireLogin =document.querySelector('.formulaire-login');
+    formulaireLogin.addEventListener("submit",function(event){
+        event.preventDefault();
 
-    //)}
-       }
+        const log = {
+            email:event.target.querySelector("[name=email-login]").value,
+            password:event.target.querySelector("[name=password-login]").value,
+        };
+        console.log(log);
+
+        const chargeUtile = JSON.stringify(log)
+        
+        fetch('http://localhost:5678/api/users/login', {
+            method:"POST",
+            body:chargeUtile,
+            headers: { "Content-Type": "application/json" }
+        })
+        .then(response => response.json())
+        
+    })
+    
+
+
+
+
+}
+   
+       
