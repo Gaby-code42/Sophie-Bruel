047c4138a400ad5a36263a72326f7901f3fe2d5e
RaphaelB <raphael.bonacina@hotmail.fr>
2024-07-28 19:42:58 +0200

    css file html modifier email password fini

--- Email&Password.js
+++ Email&Password.js
@@ -1,8 +1,8 @@
-function connexionSucces (data){
-
+function connexionSucces (data, token){
         document.getElementById('before-login').style.display ='block';
         document.getElementById('login').style.display = 'none';
         document.getElementById('text-projet').style.display ='block';
+        localStorage.setItem('authToken', token);
 }
 
 export function Emailbutton (){
@@ -11,10 +11,22 @@ export function Emailbutton (){
         event.preventDefault();
 
         const log = {
-            email:event.target.querySelector("[name=email-login]").value,
-            password:event.target.querySelector("[name=password-login]").value,
+            email:event.target.querySelector("[name=email]").value,
+            password:event.target.querySelector("[name=password]").value,
         };
         console.log(log);
+
+        const emailRegex=/^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]/;;
+        const passwordRegex=/[a-z0-9._-]/;
+
+        if (!emailRegex.test(log.email)){
+            document.getElementById('error-message').innerText = "format de l'email non conforme";
+            return;
+        }
+        if(!passwordRegex.test(log.password)){
+            document.getElementById('error-message').innerText = "format du mot de passe non conforme";
+            return;
+        }
 
         const chargeUtile = JSON.stringify(log)
         
@@ -27,15 +39,17 @@ export function Emailbutton (){
             if (response.status === 200) {
               return response.json();
             } else if (response.status === 401) {
-              throw new Error('Non autorisé. Vérifiez votre nom d\'utilisateur et votre mot de passe.');
+              throw new Error('Erreur dans l’identifiant ou le mot de passe');
             } else if (response.status === 404) {
-              throw new Error('Utilisateur non trouvé.');
+              throw new Error('Erreur dans l’identifiant ou le mot de passe');
             } else {
               throw new Error('Une erreur s\'est produite. Veuillez réessayer plus tard.');
             }
           }) 
           .then(data => {
-            connexionSucces(data);
+            const token = data.token;
+            connexionSucces(data, token);
+            console.log(data)
           })
     })
 }
