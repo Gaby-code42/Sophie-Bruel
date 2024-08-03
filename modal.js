2dfe4bc3c70cee833eebc9943b7dc3486712c1db
RaphaelB <raphael.bonacina@hotmail.fr>
2024-08-03 12:01:00 +0200

    suppression de const

--- modal.js
+++ modal.js
@@ -1,40 +1,5 @@
-let modal = null
+
 
 export function modalButton (){
 }
-
-const openModal = function(e){
-    e.preventDefault()
-    const target = document.querySelector(e.target.getAttribute('href'))
-    target.style.display = 'flex'
-    target.removeAttribute('aria-hidden')
-    target.setAttribute('aria-modal', 'true')
-    modal = target
-    modal.addEventListener('click',closeModal)
-    modal.querySelector('.js-modal-close').addEventListener('click',closeModal)
-    modal.querySelector('.js-modal-stop').addEventListener('click',stopPropagation)
-}
-
-const closeModal = function (e){
-    if (modal === null) return
-    e.preventDefault()
-    modal.style.display = "none"
-    modal.setAttribute('aria-modal','true')
-    modal.removeAttribute('aria-modal')
-    modal.removeEventListener('click',closeModal)
-    modal.querySelector('.js-modal-close').removeEventListener('click',closeModal)
-    modal.querySelector('.js-modal-stop').removeEventListener('click',stopPropagation)
-    modal = null
-
-}
-
-const stopPropagation = function(e){
-    e.stopPropagation() 
-}
-
-
-
-
-
-
 
