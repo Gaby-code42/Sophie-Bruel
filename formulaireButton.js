29e635669d1ed37caab6454e2181496b2cd42e43
RaphaelB <raphael.bonacina@hotmail.fr>
2024-08-07 15:12:22 +0200

    formulaire completed , prochain objectif envoie a l'api

--- formulaireButton.js
+++ formulaireButton.js
@@ -1,111 +1,130 @@
 const changementImg = document.getElementById('changement-img');
-let originalContent = changementImg.innerHTML
+let originalContent = changementImg.innerHTML;
+
+//variable checking//
+
+let selectedCategory = '';
+let imgAjoutModal = null;
+let inputTitle = '';
+let isImageValid = false; 
+let isCategoryValid = false; 
+let isTitleValid = false; 
 
 
-    
 
+export async function formulaireButton() {
+    const response = await fetch('http://localhost:5678/api/works');
+    const works = await response.json();
 
-export async function formulaireButton (){
-    const response = await fetch('http://localhost:5678/api/works')
-    const works = await response.json()
-    
+    //formulaire categories//
+
     const uniqueCategories = new Set();
-
     works.forEach(work => {
         uniqueCategories.add(work.category.name);
-      });
+    });
 
     const categoriesArray = Array.from(uniqueCategories);  
-    
-    console.log(categoriesArray)
 
-    const containerSelect = document.querySelector('.form-catSelect')
+    const containerSelect = document.querySelector('.form-catSelect');
+    const selectForm = document.createElement('select');
+    selectForm.id = 'category';
+    selectForm.name = 'category';
+    containerSelect.appendChild(selectForm);
 
-    const selectForm = document.createElement('select')
-    selectForm.id='category'
-    selectForm.name='category'
-    containerSelect.appendChild(selectForm)
+    const selectFunction = document.getElementById('category');
+    selectFunction.innerHTML = '';
 
-    const selectFunction = document.getElementById('category')
-
-    selectFunction.innerHTML=''
-
-    const defaultOption = document.createElement('option')
-    defaultOption.value = ''
-    defaultOption.text = ''
-    selectFunction.appendChild(defaultOption)
+    const defaultOption = document.createElement('option');
+    defaultOption.value = '';
+    defaultOption.text = '';
+    selectFunction.appendChild(defaultOption);
 
     categoriesArray.forEach(category => {
-        const option = document.createElement('option')
-        option.value = category
-        option.text = category
-        selectFunction.appendChild(option)
+        const option = document.createElement('option');
+        option.value = category;
+        option.text = category;
+        selectFunction.appendChild(option);
+    });
 
-        selectFunction.addEventListener('change', async (event) => {
-            const selectedCategory = event.target.value
-            if(selectedCategory){console.log(selectedCategory)}
-        })
-    })
+    selectFunction.addEventListener('change', (event) => {
+        selectedCategory = event.target.value;
+        isCategoryValid = !!selectedCategory;
+        checkFormCompletion();
+    });
 
-    const photoButton = document.querySelector('.photo-button')
-
-    const inputImg = document.createElement('input')
-        inputImg.type='file'
-        inputImg.accept='.jpg, .jpeg, .png'
-        inputImg.classList.add('photo-upload')
-        inputImg.id='photoUpload'
-        photoButton.appendChild(inputImg)
-
+    // formulaire Image //
+    const photoButton = document.querySelector('.photo-button');
+    const inputImg = document.createElement('input');
+    inputImg.type = 'file';
+    inputImg.accept = '.jpg, .jpeg, .png';
+    inputImg.classList.add('photo-upload');
+    inputImg.id = 'photoUpload';
+    photoButton.appendChild(inputImg);
 
     inputImg.addEventListener('change', function(event) {
+        const clickImg = event.target.files[0];
+        const fileRead = new FileReader();
 
-        const clickImg = event.target.files[0]
-        const fileRead = new FileReader()
-    
+        fileRead.onload = function(e) {
+            imgAjoutModal = document.createElement('img');
+            imgAjoutModal.src = e.target.result;
+            changementImg.innerHTML = '';
+            changementImg.appendChild(imgAjoutModal);
 
-    fileRead.onload = function(e) {
-        const imgAjoutModal = document.createElement('img');
-        imgAjoutModal.src = e.target.result;
-        changementImg.innerHTML = '';
-        changementImg.appendChild(imgAjoutModal);
+            imgAjoutModal.addEventListener('click', function() {
+                inputImg.click();
+            });
 
-        imgAjoutModal.addEventListener('click', function() {
-            inputImg.click();
-        });
-    };
+            isImageValid = !!imgAjoutModal.src;
+            checkFormCompletion();
+        };
 
-    if (clickImg) {
-        fileRead.readAsDataURL(clickImg);
-    }else{changementImg.innerHTML = originalContent;
-        formulaireButton();
-        categoryOptions()
+        if (clickImg) {
+            fileRead.readAsDataURL(clickImg);
+        } else {
+            changementImg.innerHTML = originalContent;
+            formulaireButton();
+        }
+    });
+
+    // formulaire titre //
+    const titleButton = document.querySelector('.form-catTitle');
+    const inputTitleElement = document.createElement('input');
+    inputTitleElement.type = 'text';
+    inputTitleElement.id = 'title';
+    inputTitleElement.name = 'title';
+    titleButton.appendChild(inputTitleElement);
+
+    inputTitleElement.addEventListener('change', (event) => {
+        inputTitle = event.target.value;
+        isTitleValid = !!inputTitle;
+        checkFormCompletion();
+    });
+
+    function checkFormCompletion() {
+        const submitButton = document.getElementById('form-validation');
+        if (isImageValid && isCategoryValid && isTitleValid) {
+            submitButton.disabled = false;
+        } else {
+            submitButton.disabled = true;
+        }
     }
-})
-    const titleButton = document.querySelector('.form-catTitle')
-
-    const inputTitle = document.createElement('input')
-    inputTitle.type = 'text'
-    inputTitle.id = 'title'
-    inputTitle.name = 'title'
-    titleButton.appendChild(inputTitle)
-
-    inputTitle.addEventListener('change',async (event) =>{
-        const inputTitle = event.target.value
-        if(inputTitle){console.log(inputTitle)}
-
-    })
     
-    //<button type="submit" class="submit-button" id="form-validation">Valider</button>
+    //bouton submit//
 
-    const submitCase = document.querySelector('.form-submit')
-    const inputSummit = document.createElement('button')
-    inputSummit.type = 'submit'
-    inputSummit.classList.add('submit-button')
-    inputSummit.id = 'form-validation'
-    inputSummit.innerText = "Valider"
-    submitCase.appendChild(inputSummit)
+    const submitCase = document.querySelector('.form-submit');
+    const inputSubmit = document.createElement('button');
+    inputSubmit.type = 'submit';
+    inputSubmit.classList.add('submit-button');
+    inputSubmit.id = 'form-validation';
+    inputSubmit.innerText = "Valider";
+    inputSubmit.disabled = true;
+    submitCase.appendChild(inputSubmit);
 
-    function validation (){
-
-    }
+    inputSubmit.addEventListener('click', () => {
+        if (isImageValid && isCategoryValid && isTitleValid) {
+            console.log("frero ca marche");
+            
+        }
+    });
 }
